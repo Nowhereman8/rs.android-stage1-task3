ed824167e5d4de6c1d4f41401f276233332f8a07
Maxim Poleshchuk <maxxxpol@gmail.com>
2020-05-05 22:31:36 +0300

    feat: combinator complete

--- src/main/kotlin/subtask2/Combinator.kt
+++ src/main/kotlin/subtask2/Combinator.kt
@@ -4,6 +4,26 @@ class Combinator {
 
     // TODO: Complete the following function
     fun checkChooseFromArray(array: Array<Int>): Int? {
-        throw NotImplementedError("Not implemented")
+        var mas = array
+
+        for(x in 0 .. (mas[1]/2 )){
+            if(mas[0]*fact(x)*fact(mas[1]-x) == fact(mas[1]))
+                return x
+        }
+        return null
+    }
+
+    fun fact (n : Int): Int {
+        when (n) {
+            1 -> return 1
+            0 -> return 1
+            else -> {
+                var res = 1
+                for (i in 1..n) {
+                    res *= i
+                }
+                return res
+            }
+        }
     }
 }
