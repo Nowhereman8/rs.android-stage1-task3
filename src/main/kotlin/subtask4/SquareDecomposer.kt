1668f41a5ae447016b4dc0f55fbe52a4ed2fccc7
Maxim Poleshchuk <maxxxpol@gmail.com>
2020-05-05 22:14:48 +0300

    feat: square decomposer

--- src/main/kotlin/subtask4/SquareDecomposer.kt
+++ src/main/kotlin/subtask4/SquareDecomposer.kt
@@ -1,9 +1,38 @@
 package subtask4
 
+import java.lang.Math.floor
+import java.lang.Math.sqrt
+
+
 class SquareDecomposer {
 
-    // TODO: Complete the following function
     fun decomposeNumber(number: Int): Array<Int>? {
-        throw NotImplementedError("Not implemented")
+        return decompose( number - 1, number * number)
+    }
+
+    private fun decompose(n: Int, prod: Int): Array<Int>? {
+        for (i in n downTo 1) {
+            var ost = prod - i * i
+            if (ost == 0) {
+                return arrayOf(i)
+            }
+
+            if (ost < 0) {
+                return null
+            }
+
+            var koren = floor( sqrt(ost.toDouble()) ).toInt()
+
+            if (koren >= i) {
+                koren = i - 1
+            }
+
+            var res = decompose(koren, ost)
+
+            if (res != null) {
+                return res + arrayOf(i)
+            }
+        }
+        return null
     }
 }
