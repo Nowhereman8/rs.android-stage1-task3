77de986c152e47ce37b1932ed9d95be954bf4723
Maxim Poleshchuk <maxxxpol@gmail.com>
2020-05-06 16:39:07 +0300

    feat: start 6

--- src/main/kotlin/subtask6/FullBinaryTrees.kt
+++ src/main/kotlin/subtask6/FullBinaryTrees.kt
@@ -2,8 +2,11 @@ package subtask6
 
 class FullBinaryTrees {
 
-    // TODO: Complete the following function
     fun stringForNodeCount(count: Int): String {
-        throw NotImplementedError("Not implemented")
+
+        if (count == 1) {return "[[0]]"}
+        if (count == 3) {return "[[0,0,0]]"}
+        if (count % 2 == 0) {return "[]"}
+        return "[1]"
     }
 }
